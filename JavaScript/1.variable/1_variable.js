dadc2a54b5d2cfa414f09241ac7efce3d1133795
doyoon0 <doyoon.2109@gmail.com>
2025-08-19 18:01:40 +0900

    commit

---
+++ JavaScript/1.variable/1_variable.js
@@ -0,0 +1,18 @@
+/**
+ * ECMA Script 2015 이후 버전 기본으로 정리함!!
+ * 변수 선언 키워드 : var, let, const
+ * 형식 : 키워드 변수명 = 데이터 값;
+ */
+
+//name 이라는 변수에 홍길동 저장
+let name1 = "홍길동";
+let name2 = '홍길동';
+console.log(name1);
+console.log(name2);
+
+//number 변수에 100을 저장
+let number = 100;
+let dnumber = 100.123;
+console.log(number);
+console.log(dnumber);
+
---
+++ JavaScript/1.variable/3_datatype.js
@@ -0,0 +1,5 @@
+/**
+ * Primitive Datatype (기본형)
+ * 
+ * Refernce DataType (참조형)
+ */
